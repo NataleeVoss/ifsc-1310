528338271fa25bb6377eeee332d95e4dc3875fad
NataleeVoss <nsvoss@ualr.edu>
2021-05-03 23:51:43 +0000

    Final Revisions and Reflections & Cycle Lab

---
+++ cycle/assets/js/local-calls.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+$('.gallery').cycle({
+		fx: 'turnUp', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+		speed: 1400,
+    timeout:  2000
+	});
+
+});
